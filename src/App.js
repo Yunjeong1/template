a9c6224541813e7f91d23aaa833132443c561e39
Yunjeong <8768moon@naver.com>
2022-04-07 10:12:34 +0900

    how to make components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+//불러올때는 비구조화 할당을 이용해서 원하는 컴포넌트만 선별적으로 import 가능
+import { Header, Popup } from './Common';
+
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='App'>
+			<h1>새로운 컴포넌트 만들기</h1>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,45 @@
+//선언 함수
+function Header() {
+	return (
+		<header>
+			<h1>LOGO</h1>
+			<ul id='gnb'>
+				<li>menu</li>
+				<li>menu</li>
+			</ul>
+		</header>
+	);
+}
+
+export default Header;
+
+/* 
+//대입형 함수
+const Header = () =>{
+  return (
+		<header>
+			<h1>LOGO</h1>
+			<ul id='gnb'>
+				<li>menu</li>
+				<li>menu</li>
+			</ul>
+		</header>
+	);
+}
+export default Header;
+*/
+
+/*
+//내보낼 함수가 한개일때 이렇게 쓰기도 함
+export default function Header() {
+	return (
+		<header>
+			<h1>LOGO</h1>
+			<ul id='gnb'>
+				<li>menu</li>
+				<li>menu</li>
+			</ul>
+		</header>
+	);
+}
+*/
