6f1388ce82e85318d216f2518af6a46bade7d6ee
Yunjeong <8768moon@naver.com>
2022-04-08 17:20:07 +0900

    useRef로 박스 회전하기

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import './scss/style.scss';
-import { useState, useEffect } from 'react';
+import { useState, useRef } from 'react';
+
+/*
+useRef
+- 아직 리얼돔으로 변경되지 않은 가상돔을 미리 객체에 담아서 참조
+
+const 변수 = useRef(null);
+--> current 라는 키값이 있는 객체를 반환
+--> 반환된 빈 객체를 원하는 가상돔 요소에 ref 속성으로 지정
+--> ref로 지정된 가상돔이 해당 객체에 저장
+*/
 
 function App() {
-	//버튼 클릭시 증가될 카운트값을 관리할 state
 	let [num, setNum] = useState(0);
-	//num값을 활용해서 실제 box가 회전할 각도값을 관리할 state
-	let [rot, setRot] = useState(0);
-
-	//num값이 변경될때마다 실행되는 useEffect 추가
-	useEffect(() => {
-		//num값이 변경될때마다 45를 곱해서 해당값을 다시 rot에 적용하여 변경
-		setRot(45 * num);
-		console.log('rot', rot);
-	}, [num]);
+	//가상돔이 참조될 빈 객체를 useRef로 생성
+	let box = useRef(null);
+	console.log(box);
 
 	return (
 		<>
@@ -20,16 +23,20 @@ function App() {
 				<button
 					onClick={() => {
 						setNum(--num);
+						//컴포넌트에서 참조된 가상돔을 자유롭게 호출 가능
+						box.current.style.transform = `rotate(${45 * num}deg)`;
 					}}>
 					반시계방향 회전
 				</button>
 				<button
 					onClick={() => {
 						setNum(++num);
+						box.current.style.transform = `rotate(${45 * num}deg)`;
 					}}>
 					시계방향 회전
 				</button>
-				<div className='box' style={{ transform: `rotate(${rot}deg)` }}></div>
+				{/* 참조하고싶은 가상돔의 ref속성을 추가하고 useRef로 만든 객체 연결 */}
+				<div className='box' ref={box}></div>
 			</article>
 		</>
 	);
