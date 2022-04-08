bb664be4e041a86f0383eaa6fee73baa1c317095
Yunjeong <8768moon@naver.com>
2022-04-08 16:10:57 +0900

    방금한 useEffect에 주석 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
 import './scss/style.scss';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+/*
+
+useEffect
+- 컴포넌트의 생명주기(life cycle)에 따라 특정 기능을 호출하는 hook 
+- 컴포넌트 생성, 변경, 소멸
+- useEffect(호출할 함수, [의존성 배열])
+
+- 컴포넌트 생성시
+	useEffect(()=>{
+		이곳의 구문이 처음 로딩시 한번만 호출
+	}, [])
+	
+- 컴포넌트 소멸시
+	useEffect(()=>{
+		return() =>{
+			컴포넌트가 사라질때 한번만 호출
+		}
+	},[])
+
+-컴포넌트 변경시
+	useEffect(()=>{
+		의존성배열에 등록한 state값이 변경될때마다 호출
+	}, [state])
+*/
 
 function App() {
 	let [num, setNum] = useState(0);
-
 	//변경할 배경색을 관리할 state추가 (초기값은 gray)
 	let [bg, setBg] = useState('gray');
+
+	//의존성배열이 비어있으므로 처음 컴포넌트 실행시 한번만 호출
+	useEffect(() => {
+		console.log('App생성');
+
+		//의존성배열이 비어있으므로 컴포넌트 소멸시 한번만 호출
+		return () => {
+			console.log('App소멸');
+		};
+	}, []);
+
+	//state값(num)이 변경될때마다 호출
+	useEffect(() => {
+		console.log('num값 변경');
+	}, [num]);
+
+	useEffect(() => {
+		console.log('bg값 변경');
+	}, [bg]);
 
 	return (
 		<>
--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+	//<React.StrictMode>
+	<App />
+	//</React.StrictMode>
 );
+
+// 리액트에서 strict모드일때는 컴포넌트가 return문 실행이 될때에도 useEffect 호출
+//strict모드를 해제하면 useEffect문이 한번씩만 호출
