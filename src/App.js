adfdffc104690f728310394c0b52a522b0af7697
Yunjeong <8768moon@naver.com>
2022-04-08 15:24:46 +0900

    방금한 예제에 주석 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { useState } from 'react';
 
 function App() {
 	let [num, setNum] = useState(0);
+
+	//변경할 배경색을 관리할 state추가 (초기값은 gray)
 	let [bg, setBg] = useState('gray');
 
 	return (
@@ -30,6 +32,8 @@ function App() {
 			<article className='colorBox'>
 				<div className='box' style={{ backgroundColor: bg }}></div>
 
+				{/* 이벤트 연결 */}
+				{/* <button onClick={e => {setBg('red');}}>red</button> */}
 				<button
 					onClick={(e) => {
 						setBg(e.target.innerText);
